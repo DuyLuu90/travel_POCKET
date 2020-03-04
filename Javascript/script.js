645532e7f7af0de1c7dc7a279f159b50654e4b3e
gdreid13 <gdreid13@gmail.com>
2020-03-03 21:30:54 -0800

    first steps towards making the knowledge API work

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,22 +1,64 @@
 'use strict';
+
+
+
 let searchedTerm='';
 
-const wikiEndpoint='http://en.wikipedia.org/w/api.php?origin=*&action=opensearch&format=json&search='
+const googleSearchUrl = "https://kgsearch.googleapis.com/v1/entities:search";
+
+const google_api_key = "AIzaSyAe5uuD2UQozJY02zt42HAFnCCeWq-nd8s"
+
+// const wikiEndpoint='http://en.wikipedia.org/w/api.php?origin=*&action=opensearch&format=json&search='
+
+function getCountryCode(searchedTerm) {
 
 
-function handleSeachButton() {
+  const googleSearchTerm = '';
+  
+}
+
+
+function renderHomePage() {
+
+}
+
+
+function getKnowledgeData(googleSearchTerm) {
+
+    let params = {
+      key: google_api_key,
+      query: googleSearchTerm,
+      types: "City",
+    }
+
+    const queryString = $.param(params);
+    const url = `${googleSearchUrl}?${queryString}`;
+    fetch(url).then(resp => {
+      if(resp.ok) {
+        return resp.json();
+      }
+
+      throw new Error(resp.statusText);
+    }).then(respJson=>displayCityResults(respJson))
+
+
+}
+
+
+
+function handleSearchButton() {
   $('#search').submit(event => {
     event.preventDefault();
-    let searchedTerm=$('#search-box').val().toUpperCase();
-    let html= renderHomePage(searchedTerm);
+    const searchedTerm=$('#search-box').val().toUpperCase();
+    renderHomePage(searchedTerm);
     $('main').html(`${html}`);
   })  
 }
 
 function runApp() {
-  handleSeachButton();
+  handleSearchButton();
   displayCountries();
-  getCountryCode ();
+  getCountryCode();
 }
 
 $(runApp);
