35319911424b5728ec75adbb1a0381a69226082a
gdreid13 <gdreid13@gmail.com>
2020-03-04 16:33:51 -0800

    debugging page render

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,7 +2,6 @@
 
 
 
-let searchedTerm='';
 
 const googleSearchUrl = "https://kgsearch.googleapis.com/v1/entities:search";
 
@@ -10,20 +9,34 @@ const google_api_key = "AIzaSyAe5uuD2UQozJY02zt42HAFnCCeWq-nd8s"
 
 // const wikiEndpoint='http://en.wikipedia.org/w/api.php?origin=*&action=opensearch&format=json&search='
 
-function getCountryCode(searchedTerm) {
+/* function getCountryCode(searchedTerm) {
 
 
   const googleSearchTerm = '';
 
-}
+} */
 
+function displayCityResults(json) {
 
-function renderHomePage() {
+  $("#knowledge-bar-results").empty();
+
+  console.log(json);
+
+  $("#knowledge-bar-results").append(
+      `<li>
+          <h3>${json.itemListElement.result.name}</h3>
+          <p>${json.itemListElement.result.detailedDescription.articleBody}</p>
+          <p><img src='${json.itemListElement.result.image.contentUrl}'  alt='Picture of ${json.itemListElement.result.name}></p>
+          `
+  )
+  $("knowledge-bar").removeClass('hidden');
 
 }
 
 
 function getKnowledgeData(googleSearchTerm) {
+
+    console.log('Getting knowledge panel data...');
 
     let params = {
       key: google_api_key,
@@ -31,8 +44,8 @@ function getKnowledgeData(googleSearchTerm) {
       types: "City",
     }
 
-    const queryString = $.param(params);
-    const url = `${googleSearchUrl}?${queryString}`;
+    const googleQueryString = $.param(params);
+    const url = `${googleSearchUrl}?${googleQueryString}`;
     fetch(url).then(resp => {
       if(resp.ok) {
         return resp.json();
@@ -44,29 +57,33 @@ function getKnowledgeData(googleSearchTerm) {
 
 }
 
-function displayCityResults(json) {
 
-    $("#knowledge-bar-results").empty();
 
-    $("#knowledge-bar-results").append(
-        `<li>
-            <h3>${json.itemListElement.result.name}</h3>
-            <p>${json.itemListElement.result.detailedDescription.articleBody}</p>
-            <p><img src='${json.itemListElement.result.image.contentUrl}'  alt='Picture of ${json.itemListElement.result.name}></p>
-            `
-    )
-    $("knowledge-bar").removeClass('hidden');
+function renderHomePage(searchedTerm) {
+
+    
+    console.log(`Looking for ${searchedTerm}...`);
+
+    const googleSearchTerm = `${searchedTerm}, France`;
+    getKnowledgeData(googleSearchTerm);
 
 }
 
 
-
 function handleSearchButton() {
+
+    console.log('Handling search button!');
+
     $('#search').submit(event => {
         event.preventDefault();
         const searchedTerm=$('#search-box').val().toUpperCase();
+
+        console.log(`Search for ${searchedTerm} is on!`);
+        
         renderHomePage(searchedTerm);
-        $('main').html(`${html}`);
+
+        console.log('Just called renderHomePage...');
+//        $('main').html(`${html}`);
     })  
 }
 
